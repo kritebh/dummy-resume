9366e32fb62d98c14fc510e977c3bdcd7dcc99c7
Kritebh Lagan Bibhakar <54534607+kritebh@users.noreply.github.com>
2021-02-04 01:45:45 +0530

    Updated

--- resume/settings.py
+++ resume/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')kshh9xri2wt=th46+t*2+$n^ol)pw7e^qfy&z@fsgkbkgb=tn'
+SECRET_KEY = 't=th46+t*2+$*'4327n^ol)pw7e^q+fy&z@fsgkbkgb=tn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
